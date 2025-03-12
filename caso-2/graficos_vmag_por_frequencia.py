569215dcb5fc79a46776ffeac7f8aec238273ee8
Alfa Marine <alfamarine.ferreira@gmail.com>
2025-03-12 09:24:51 -0300

    add arquivos csv de V do caso 2

--- caso-2/graficos_vmag_por_frequencia.py
+++ caso-2/graficos_vmag_por_frequencia.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-vmag_df = pd.read_csv("./vmag_caso1_node_828.1.csv", index_col=0)
+vmag_df = pd.read_csv("./vmag_caso2_node_828.1.csv", index_col=0)
 tensao_1 = vmag_df.iloc[59,:]
 tensao_2 = vmag_df.iloc[60,:]
 tensao_3 = vmag_df.iloc[61,:]
