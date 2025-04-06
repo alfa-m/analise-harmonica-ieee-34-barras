b9284c9a58cddd23877197e9efc89f094e552794
Alfa Marine <alfamarine.ferreira@gmail.com>
2025-04-06 20:16:19 -0300

    add mais códigos python do caso 1

--- caso-1/graficos_vmag_por_frequencia.py
+++ caso-1/graficos_vmag_por_frequencia.py
@@ -2,26 +2,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dados = pd.read_csv('./vmag_caso1_node_832.1.csv', index_col = 0)
+dados = pd.read_csv('./vmag_caso1_node_802.1.csv', index_col = 0)
 dados_sem_harmonico_1 = dados.iloc[:,2:]
 dados_transpostos = dados_sem_harmonico_1.T
 harmonicos = np.arange(61,3000.5,0.5)
 plt.plot(harmonicos, dados_transpostos)
 plt.xlabel("Frequência (Hz)")
 plt.xlim(0,3000)
-plt.ylabel("Impedância (V)")
-plt.ylim(0,1000)
-plt.title("Fonte harmônica no nó 832.1")
+plt.ylabel("Tensão (V)")
+plt.ylim(0,5000)
+plt.title("Fonte harmônica no nó 802.1")
 plt.grid()
-plt.savefig("./vmag_caso1_no_832_sem_harmonico_1_com_eixo.png")
+plt.savefig("./vmag_caso1_no_802_sem_harmonico_1_com_eixo.png")
 plt.show()
 plt.close()
 
 dados_transpostos.plot()
 plt.xlabel("Frequência (Hz)")
-plt.ylabel("Impedância (V)")
-plt.title("Fonte harmônica no nó 832.1")
+plt.ylabel("Tensão (V)")
+plt.title("Fonte harmônica no nó 802.1")
 plt.grid()
-plt.savefig("./vmag_caso1_no_832_sem_harmonico_1_sem_eixo.png")
+plt.savefig("./vmag_caso1_no_802_sem_harmonico_1_sem_eixo.png")
 plt.show()
 plt.close()
--- caso-1/graficos_vmag_por_frequencia_total.py
+++ caso-1/graficos_vmag_por_frequencia_total.py
@@ -2,25 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dados = pd.read_csv('./vmag_caso1_node_832.1.csv', index_col = 0)
+dados = pd.read_csv('./vmag_caso1_node_802.1.csv', index_col = 0)
 dados_transpostos = dados.T
 harmonicos = np.arange(60,3000.5,0.5)
 plt.plot(harmonicos, dados_transpostos)
 plt.xlabel("Frequência (Hz)")
 plt.xlim(0,3000)
-plt.ylabel("Impedância (V)")
+plt.ylabel("Tensão (V)")
 plt.ylim(0,45000)
-plt.title("Fonte harmônica no nó 832.1")
+plt.title("Fonte harmônica no nó 802.1")
 plt.grid()
-plt.savefig("./vmag_caso1_no_832_total_com_eixo.png")
+plt.savefig("./vmag_caso1_no_802_total_com_eixo.png")
 plt.show()
 plt.close()
 
 dados_transpostos.plot()
 plt.xlabel("Frequência (Hz)")
-plt.ylabel("Impedância (V)")
-plt.title("Fonte harmônica no nó 832.1")
+plt.ylabel("Tensão (V)")
+plt.title("Fonte harmônica no nó 802.1")
 plt.grid()
-plt.savefig("./vmag_caso1_no_832_total_sem_eixo.png")
+plt.savefig("./vmag_caso1_no_802_total_sem_eixo.png")
 plt.show()
 plt.close()
